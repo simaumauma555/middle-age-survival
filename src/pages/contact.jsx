a6ff5dfa9fabe8db7c82ff46866ae8f36b75d9ce
simaumauma555 <simaumauma555@gmail.com>
2021-03-12 22:29:49 +0900

    update

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -8,6 +8,7 @@ const ContactPage = center => (
    <Container center={center}>
     <h1>Contact Form</h1>
     <form name="contact" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="contact" />
     <p>
       <label>Your Name: <input type="text" name="name" /></label>   
     </p>
