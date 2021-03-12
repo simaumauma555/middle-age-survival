3ee0880b873a4e1114446031508ca11fbb6b6038
simaumauma555 <simaumauma555@gmail.com>
2021-03-12 22:31:22 +0900

    update

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -33,10 +33,10 @@ const NavBar = () => (
       {/* <img src={logo} alt="Gatsby Logo" /> ##左上のロゴをコメントアウトで削除 */}
     </StyledLink>
     <Nav>
-      <Link to="/blog">全記事</Link>
-      <Link to="/tags/英語・海外">英語・海外</Link>
-      <Link to="/tags/キャリア">キャリア</Link>
-      <Link to="/tags/資産形成">資産形成</Link>
+      <Link to="/blog">All</Link>
+      <Link to="/tags/英語・海外">English/Global</Link>
+      <Link to="/tags/キャリア">Career</Link>
+      <Link to="/tags/資産形成">Investment</Link>
       <Link to="/about">About</Link>
       {/* <Link to="/privacy-policy">プライバシーポリシー</Link> */}
     </Nav>
